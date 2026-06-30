f2cfd74ee43e2bdc56a3f29f8ca6aa43774cea05
Greg Rozmarynowycz <thundercracker15@gmail.com>
2026-06-30 12:05:00 +0200

    remove remaining zone js code

--- packages/wf-core/src/test-setup.ts
+++ packages/wf-core/src/test-setup.ts
@@ -1,5 +1,3 @@
-import '@analogjs/vitest-angular/setup-zone';
-
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
